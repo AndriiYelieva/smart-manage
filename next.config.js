66d1791b225f39852679db1862efb1bb70e6e6ec
AndriiYelieva <andrew.yelieva@gmail.com>
2023-12-10 09:02:00 +0200

    delete stastic

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const path = require('path')
 
 const nextConfig = {
-  output: 'export',
-  basePath: '/dzen-test-task',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
